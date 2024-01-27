bd63f57954d74c7aae6397d4801679b3ff2beca3
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2024-01-27 13:25:18 -0600

    Finished cook-off results

--- Scenes/cook_off.gd
+++ Scenes/cook_off.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var opponents = []
 var index = "Gym Buff"
 const jokes = {
 		"Gym Buff": ["What do gymnasts eat every morning? A balanced breakfast.",
@@ -45,8 +46,41 @@ var dadPics = {
 	"Unhealthy": load("res://Assets/Sprites/Dads/unhealthy_dad.png"),
 	"Mid-Life Crisis": load("res://Assets/Sprites/Dads/Mid_life_crisis_dad.png")
 }
+
+var dadFood = [
+	load("res://Assets/Sprites/Dishes/plate_1.png"),
+	load("res://Assets/Sprites/Dishes/plate_2.png"),
+	load("res://Assets/Sprites/Dishes/plate_3.png"),
+	load("res://Assets/Sprites/Dishes/plate_4.png"),
+	load("res://Assets/Sprites/Dishes/plate_5.png"),
+	load("res://Assets/Sprites/Dishes/plate_6.png")
+]
+
+var medals = [
+	load("res://Assets/Sprites/Medals/First_place.png"),
+	load("res://Assets/Sprites/Medals/Second_place.png"),
+	load("res://Assets/Sprites/Medals/Third_place.png")
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var score = 10
+	if score == 10:
+		opponents = [2,3,4]
+		opponents.shuffle()
+		opponents.append(1)
+	elif score == 9:
+		opponents = [1,3,4]
+		opponents.shuffle()
+		opponents.append(2)
+	elif score >= 7:
+		opponents = [2,1,4]
+		opponents.shuffle()
+		opponents.append(3)
+	else:
+		opponents = [2,1,3]
+		opponents.shuffle()
+		opponents.append(4)
 	$UI/Dad1/Label.visible=false
 	$UI/Dad2/Label.visible=false
 	$UI/Dad3/Label.visible=false
@@ -55,6 +89,24 @@ func _ready():
 	$UI/Dad4/Joke1.visible=false
 	$UI/Dad4/Joke2.visible=false
 	$UI/Dad4/Joke3.visible=false
+	$UI/Dad1/Dish.visible=false
+	$UI/Dad2/Dish.visible=false
+	$UI/Dad3/Dish.visible=false
+	$UI/Dad4/Dish.visible=false
+	$UI/Dad1/Medal.visible=false
+	$UI/Dad2/Medal.visible=false
+	$UI/Dad3/Medal.visible=false
+	$UI/Dad4/Medal.visible=false
+	var foodCopy = dadFood
+	var foods = []
+	while len(foods) < 4:
+		var newFood = randi()%len(foodCopy)
+		foods.append(foodCopy[newFood])
+		foodCopy.remove_at(newFood)
+	$UI/Dad1/Dish.texture = foods[0]
+	$UI/Dad2/Dish.texture = foods[1]
+	$UI/Dad3/Dish.texture = foods[2]
+	$UI/Dad4/Dish.texture = foods[3]
 	var dads = jokes.keys()
 	index = randi() % dads.size()
 	$UI/Dad1/Label.text = jokes[dads[index]][randi()%3]
@@ -79,10 +131,13 @@ func _ready():
 	dads.remove_at(index)
 	await get_tree().create_timer(1.0).timeout
 	$UI/Dad1/Label.visible=true
+	$UI/Dad1/Dish.visible=true
 	await get_tree().create_timer(3.0).timeout
 	$UI/Dad2/Label.visible=true
+	$UI/Dad2/Dish.visible=true
 	await get_tree().create_timer(3.0).timeout
 	$UI/Dad3/Label.visible=true
+	$UI/Dad3/Dish.visible=true
 	await get_tree().create_timer(3.0).timeout
 	$UI/Dad4/Pick.visible=true
 	$UI/Dad4/Joke1.visible=true
@@ -112,23 +167,70 @@ func _process(delta):
 func _on_joke_1_pressed():
 	$UI/Dad4/Label.text = jokes[$UI/Dad4/Joke1.text][randi()%3]
 	$UI/Dad4/Label.visible = true
+	$UI/Dad4/Dish.visible=true
 	$UI/Dad4/Pick.visible = false
 	$UI/Dad4/Joke1.visible = false
 	$UI/Dad4/Joke2.visible = false
 	$UI/Dad4/Joke3.visible = false
+	results()
 
 func _on_joke_2_pressed():
 	$UI/Dad4/Label.text = jokes[$UI/Dad4/Joke2.text][randi()%3]
 	$UI/Dad4/Label.visible = true
+	$UI/Dad4/Dish.visible=true
 	$UI/Dad4/Pick.visible = false
 	$UI/Dad4/Joke1.visible = false
 	$UI/Dad4/Joke2.visible = false
 	$UI/Dad4/Joke3.visible = false
+	results()
 	
 func _on_joke_3_pressed():
 	$UI/Dad4/Label.text = jokes[$UI/Dad4/Joke3.text][randi()%3]
 	$UI/Dad4/Label.visible = true
+	$UI/Dad4/Dish.visible=true
 	$UI/Dad4/Pick.visible = false
 	$UI/Dad4/Joke1.visible = false
 	$UI/Dad4/Joke2.visible = false
 	$UI/Dad4/Joke3.visible = false
+	results()
+
+func results():
+	await get_tree().create_timer(3.0).timeout
+	if opponents[0]==3:
+		$UI/Dad1/Medal.texture = medals[2]
+		$UI/Dad1/Medal.visible = true
+	if opponents[1]==3:
+		$UI/Dad2/Medal.texture = medals[2]
+		$UI/Dad2/Medal.visible = true
+	if opponents[2]==3:
+		$UI/Dad3/Medal.texture = medals[2]
+		$UI/Dad3/Medal.visible = true
+	if opponents[3]==3:
+		$UI/Dad4/Medal.texture = medals[2]
+		$UI/Dad4/Medal.visible = true
+	await get_tree().create_timer(1.0).timeout
+	if opponents[0]==2:
+		$UI/Dad1/Medal.texture = medals[1]
+		$UI/Dad1/Medal.visible = true
+	if opponents[1]==2:
+		$UI/Dad2/Medal.texture = medals[1]
+		$UI/Dad2/Medal.visible = true
+	if opponents[2]==2:
+		$UI/Dad3/Medal.texture = medals[1]
+		$UI/Dad3/Medal.visible = true
+	if opponents[3]==2:
+		$UI/Dad4/Medal.texture = medals[1]
+		$UI/Dad4/Medal.visible = true
+	await get_tree().create_timer(1.0).timeout
+	if opponents[0]==1:
+		$UI/Dad1/Medal.texture = medals[0]
+		$UI/Dad1/Medal.visible = true
+	if opponents[1]==1:
+		$UI/Dad2/Medal.texture = medals[0]
+		$UI/Dad2/Medal.visible = true
+	if opponents[2]==1:
+		$UI/Dad3/Medal.texture = medals[0]
+		$UI/Dad3/Medal.visible = true
+	if opponents[3]==1:
+		$UI/Dad4/Medal.texture = medals[0]
+		$UI/Dad4/Medal.visible = true
