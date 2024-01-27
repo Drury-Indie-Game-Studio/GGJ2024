e463ee8b051bbacb1799f1e6066f5fa811eb8bcb
TaylorBD-314 <taylor.boese@gmail.com>
2024-01-27 04:14:05 -0600

    Play button animation

---
+++ Scenes/PlayButton.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
+
+
+func _on_mouse_entered():
+	get_node("AnimatedSprite2D").play("Focus")
+
+
+func _on_mouse_exited():
+	get_node("AnimatedSprite2D").play("Idle")
